87a55b5b61bb45506bb0d52cd64f458c949fd96a
jjdiaz <jj.diaz@globant.com>
2016-06-23 14:49:44 -0300

    Working in exercise 3

--- exercise3/course_list.js
+++ exercise3/course_list.js
@@ -4,13 +4,12 @@ let Course = require('./models/course');
 
 var courseList = [];
 
-/*
 let maths = new Course('Maths', 8),
     chemestry = new Course('Chemestry', 7),
     javascript = new Course('JavaScript', 6);
 
 courseList.push(maths);
 courseList.push(chemestry);
-courseList.push(javascript);*/
+courseList.push(javascript);
 
 module.exports = courseList;
